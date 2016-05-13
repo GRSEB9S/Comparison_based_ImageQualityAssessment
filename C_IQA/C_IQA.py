66b295443887d481f0266e74f12fdd0197f2b278
Haoyi Liang <hoy@Haoyis-MBP-2.san.rr.com>
2016-05-13 10:31:49 -0700

    what's this?

--- C_IQA/C_IQA.py
+++ C_IQA/C_IQA.py
@@ -1,18 +1,21 @@
-# Comparison-based Image Quality Assessment
+# Comparison-based Image Quality Assessment (This version consider the sensitivity difference to the same distortion)
+# This is the texture-based C-IQA (CT_IQA)
 # input: image1, image2
 # output: relative quality of image1 based on image2
 import cv2
 import numpy as np
 
+
 class ComparisonIQA:
-    def __init__(self, thresh = 0.12):
+    def __init__(self, thresh=0.12):
         self.thresh = thresh
-    def C_IQA(self,img1,img2):
+
+    def c_iqa(self, img1, img2):
         dif = np.mean(img1) - np.mean(img2)
-        return dif
+        return dif < self.thresh
 
 if __name__ == '__main__':
     CQ_ind = ComparisonIQA()
     img1 = cv2.imread('1_org.png')
     img2 = cv2.imread('1_pclr.png')
-    print('The quality of image1 based image2 is {}').format(CQ_ind.C_IQA(img1,img2))
+    print('The quality of image1 based image2 is {}').format(CQ_ind.c_iqa(img1, img2))
